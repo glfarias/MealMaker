ee50311e39c710d2aee6a2c4383d72334f135d4b
Gabriel <luizfariasgabriel@gmail.com>
2025-07-30 20:00:08 +0200

    refactor: unify all choose functions (dry) and replace onclick for event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,11 @@ let address;
 
 const buttonConfirm = document.getElementById("button-confirm");
 const buttonGoBack = document.getElementById("button-goback");
+const allProducts = document.querySelectorAll('.product');
+
+allProducts.forEach((product) => {
+    product.addEventListener('click', choose)
+})
 
 buttonConfirm.addEventListener('click', () => {
     sendOrder();
@@ -32,18 +37,6 @@ function totalPrice() {
     return `${(parseFloat(order.food.price) + parseFloat(order.drink.price) + parseFloat(order.dessert.price)).toFixed(2)}€`;
 }
 
-function sendOrder() {
-    customer = prompt("Please, what is your name?");
-    address = prompt("What is the address we should delivery?")
-
-    if (customer && address && document.querySelectorAll('.selected').length == 3) {
-        let message = `Hello! I'd like to order:\n- ${order.food.name} (${order.food.price})\n- ${order.drink.name} (${order.drink.price})\n- ${order.dessert.name} (${order.dessert.price})\n*TOTAL: ${totalPrice()}*\n\nMy name is ${customer.trim()}\nPlease deliver to ${address.trim()}`;
-        window.open("https://wa.me/14155238886?text="+encodeURIComponent(message));
-    } else {
-        alert("Both name and address fields are required, please try again.")
-    }
-}
-
 function checkOrderComplete() {
     if (order.food.name && order.drink.name && order.dessert.name) {
         let sendButton = document.querySelector(".send");
@@ -55,48 +48,36 @@ function checkOrderComplete() {
     }
 }
 
-function chooseFood(element) {
-    if (order.food.name && order.food.price) {
-        let entireFoodSection = document.querySelector('.food .container');
-        let elementSelected = entireFoodSection.querySelector(".selected");
-        elementSelected.classList.remove("selected");
-    }
-    
-    order.food.name = element.querySelector('.plate').innerHTML;
-    order.food.price = element.querySelector('.price').innerHTML;
+function choose(event) {
+    const categories = ['food', 'drink', 'dessert'];
+    const sectionClicked = event.target.closest('section');
+    const productClicked = event.target.closest('.product');
+    const nameClicked = productClicked.querySelector('p.plate').innerHTML
+    const priceClicked = productClicked.querySelector('p.price').innerHTML;
 
-    element.classList.toggle('selected');
-    console.log(order.food.name, order.food.price);
-    checkOrderComplete();
-}
+    // RUN 3 TIMES, ONLY 1 IS GONNA BE TRUE
+    for (let i = 0 ; i < categories.length ; i++) {
 
-function chooseDrink(element) {
-    if (order.drink.name && order.drink.price) {
-        let entireDrinkSection = document.querySelector('.drink .container');
-        let elementSelected = entireDrinkSection.querySelector(".selected");
-        elementSelected.classList.remove("selected");
-    }
+        if (sectionClicked.classList.contains(`${categories[i]}`)) {
+            // EVERYTHING BELOW WILL RUN ONLY ONCE
 
-    order.drink.name = element.querySelector('.plate').innerHTML;
-    order.drink.price = element.querySelector('.price').innerHTML;
+            // CHECK IF THERE IS AN ELEMENT SELECTED, IF YES REMOVES THE CLASS SELECTED FROM IT
+            containerOfSectionClicked = sectionClicked.querySelector('.container');
+            elementSelected = containerOfSectionClicked.querySelector('.selected');
+            if (elementSelected) {
+                elementSelected.classList.remove('selected');
+            }
 
-    element.classList.toggle('selected');
-    console.log(order.drink.name, order.drink.price);
-    checkOrderComplete();
-}
+            // ADDS THE CLASS SELECTED TO THE PRODUCT CLICKED
+            productClicked.classList.add('selected');
 
-function chooseDessert(element) {
-    if (order.dessert.name && order.dessert.price) {
-        let entireDessertSection = document.querySelector('.dessert .container');
-        let elementSelected = entireDessertSection.querySelector(".selected");
-        elementSelected.classList.remove("selected");
+            const item = order[categories[i]];
+            item.name = nameClicked;
+            item.price = priceClicked;
+        }
+
     }
 
-    order.dessert.name = element.querySelector('.plate').innerHTML;
-    order.dessert.price = element.querySelector('.price').innerHTML;
-
-    element.classList.toggle('selected');
-    console.log(order.dessert.name, order.dessert.price);
     checkOrderComplete();
 }
 
@@ -121,4 +102,16 @@ function showPopUp() {
     `;
     const popUp = document.querySelector(".popup-container");
     popUp.style.display = "block";
+}
+
+function sendOrder() {
+    customer = prompt("Please, what is your name?");
+    address = prompt("What is the address we should delivery?")
+
+    if (customer && address && document.querySelectorAll('.selected').length == 3) {
+        let message = `Hello! I'd like to order:\n- ${order.food.name} (${order.food.price})\n- ${order.drink.name} (${order.drink.price})\n- ${order.dessert.name} (${order.dessert.price})\n*TOTAL: ${totalPrice()}*\n\nMy name is ${customer.trim()}\nPlease deliver to ${address.trim()}`;
+        window.open("https://wa.me/14155238886?text="+encodeURIComponent(message));
+    } else {
+        alert("Both name and address fields are required, please try again.")
+    }
 }
